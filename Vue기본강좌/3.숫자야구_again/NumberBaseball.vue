b2f294b83af3f734ef0673307813a537c24abd37
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-03 18:40:47 +0900

    Vue 기본 강좌 3-2. 프로젝트 구조와 웹팩 빌드 + Vue 기본 강좌 3-3. 웹팩 로더 사용하기 again 🔨

---
+++ Vue기본강좌/3.숫자야구_again/NumberBaseball.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>{{result}}</h1>
+        <form v-on:submit="onSubmitForm"></form>
+        <input ref="answer" maxlength="4" v-model="value" />
+        <button>입력</button>
+    </div>
+
+<!--    <div>시도 : {{}}</div>-->
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                value: '',
+                result: '',
+            }
+        },
+        methods:{
+            onSubmitForm(e){
+                e.preventDefault();
+            }
+        },
+    };
+</script>
+
+<style>
+
+</style>
