d376b12e7660e4bf4d3d9b57a976ee7ce64d6d94
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-09 01:40:41 +0900

    Vue 기본 강좌 4-1. 반응속도체크와 webpack watch ✔
      "scripts": {
        "build": "webpack --watch"
      },
    
    --watch를 붙여서 자동으로 build 되게 할 수 있다

---
+++ Vue기본강좌/4.반응속도체크/Responsecheck.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div id="screen">{{message}}</div>
+        <div>
+            <div>평균시간 : {{}}</div>
+            <button @click="onReset">리셋</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    const getNumbers = () => {
+        const candidates = [1,2,3,4,5,6,7,8,9];
+        const array = [];
+        for(let i = 0; i<4; i+=1){
+            const chosen = candidates.splice(Math.floor(Math.random() * (9-i)),1)[0];
+            array.push(chosen);
+        }
+        return array;
+    };
+    export default {
+        data(){ //화면에 보여지는 것은 data
+            return{
+                result:[],
+                state: 'waiting',
+                message: '클릭해서 시작하세요.',
+            }
+        },
+        methods:{
+            onReset(){
+
+            }
+        }
+    };
+</script>
+
+<style>
+
+</style>
---
+++ Vue기본강좌/4.반응속도체크/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import Responsecheck from "./Responsecheck.vue";
+
+new Vue(Responsecheck).$mount('#root');
+
