4531d3d145fd6646d7abae86c7e05183fe7d611e
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-09 22:12:27 +0900

    Vue 기본 강좌 4-4. 반응속도체크 게임 완성하기 ✔

--- Vue기본강좌/4.반응속도체크/Responsecheck.vue
+++ Vue기본강좌/4.반응속도체크/Responsecheck.vue
@@ -2,13 +2,16 @@
     <div>
         <div id="screen" v-bind:class="state" @click="onClickScreen">{{message}}</div>
         <div>
-            <div>평균시간 : {{}}</div>
+            <div>평균시간 : {{result.reduce((a,c) => a+c,0)/result.length || 0}}MS</div>
             <button @click="onReset">리셋</button>
         </div>
     </div>
 </template>
 
 <script>
+    let startTime = 0;
+    let endTime = 0;
+    let timeout = null;
     const getNumbers = () => {
         const candidates = [1,2,3,4,5,6,7,8,9];
         const array = [];
@@ -28,15 +31,26 @@
         },
         methods:{
             onReset(){
-
+                this.result = [];
             },
             onClickScreen(){
                 if(this.state === 'waiting'){
                     this.state = 'ready';
+                    this.message = '초록색이 되면 클릭하세요.';
+                    timeout = setTimeout(()=>{
+                        this.state = 'now';
+                        this.message = "지금클릭하세요!";
+                        startTime = new Date();
+                    }, Math.floor(Math.random() * 1000)+2000); // 2-3초
                 } else if(this.state === 'ready'){
-                    this.state = 'now';
-                } else if(this.state === 'now'){
+                    clearTimeout(timeout);
                     this.state = 'waiting';
+                    this.message = "너무 성급하시군요! 초록색이 된 후에 클릭하세요.";
+                } else if(this.state === 'now'){
+                    endTime = new Date();
+                    this.state = 'waiting';
+                    this.message = "클릭해서 시작하세요.";
+                    this.result.push(endTime-startTime);
                 }
             },
         }
