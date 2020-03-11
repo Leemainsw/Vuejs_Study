0e91c4d569a75925456ff3fa578a0ee248912d25
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-12 00:26:46 +0900

    Vue 기본 강좌 5-1. vue-devtools와 기타 정보들 ✔
    배포 환경에서는  vue-devtools를 못보게 해야 보안에 좋다
    Vue.config.devtools = false;

---
+++ Vue기본강좌/4.반응속도체크/RockScissorsPaper.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data(){ //화면에 보여지는 것은 data
+            return{
+            };
+        },
+
+        computed:{
+        },
+
+        methods:{
+        },
+    };
+</script>
+
+<style scoped>
+    #computer{
+        width: 142px;
+        height: 200px;
+        background-position: 0 0;
+    }
+
+</style>
