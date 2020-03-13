cbd0c7a1493f88c54cce782c24ffad0d55aac179
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-13 21:08:39 +0900

    Vue 기본 강좌 5-2. :class와 :style ✔
    :class , : style
    원래는 v-bind:class, v-bind:style 형식

--- Vue기본강좌/4.반응속도체크/RockScissorsPaper.vue
+++ Vue기본강좌/4.반응속도체크/RockScissorsPaper.vue
@@ -1,21 +1,46 @@
 <template>
     <div>
-
-    </div>
+        <div id="computer" :style="computedStyleObject"></div>
+        <div>
+            <button @click="onClickButton('바위')">바위</button>
+            <button @click="onClickButton('가위')">가위</button>
+            <button @click="onClickButton('보')">보</button>
+        </div>
+            <div>{{result}}</div>
+            <div>현재 {{score}}</div>
+        </div>
 </template>
 
 <script>
 
+    const rspCords = {
+        바위 :  '0',
+        가위 : '-142px',
+        보 : '-284px',
+    }
+
     export default {
         data(){ //화면에 보여지는 것은 data
             return{
+                imgCord: rspCords.바위,
+                result: '',
+                score: 0,
             };
         },
 
         computed:{
+            computedStyleObject() {
+                return {
+                    background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCord} 0`,
+                }
+            }
+
         },
 
         methods:{
+            onClickButton(choice){
+
+            }
         },
     };
 </script>
