a20adefbe470bd8e4310e54648c0e8e572950664
Leesw_0811 <s2018s15@e-mirim.hs.kr>
2020-03-14 07:37:52 +0900

    Vue 기본 강좌 5-4. 가위바위보 완성과 devtools 분석 ✔

--- Vue기본강좌/4.반응속도체크/RockScissorsPaper.vue
+++ Vue기본강좌/4.반응속도체크/RockScissorsPaper.vue
@@ -17,7 +17,19 @@
         바위 :  '0',
         가위 : '-142px',
         보 : '-284px',
+    };
+
+    const scores = {
+        가위: 1,
+        바위: 0,
+        보: -1,
     }
+
+    const computerChoice = (imgCord) =>{
+        return Object.entries(rspCords).find(function(v){
+            return v[1] === imgCord;
+        })[0];
+    };
 
     let interval = null;
 
@@ -40,30 +52,45 @@
         },
 
         methods:{
+            changeHand(){
+                interval = setInterval(() => {
+                    if(this.imgCord === rspCords.바위){
+                        this.imgCord = rspCords.가위;
+                    } else if(this.imgCord === rspCords.가위){
+                        this.imgCord = rspCords.보;
+                    } else if(this.imgCord === rspCords.보){
+                        this.imgCord = rspCords.바위;
+                    }
+                }, 100);
+            },
             onClickButton(choice){
-
+                clearInterval(interval);
+                const myScore = scores[choice];
+                const cpuScore = scores[computerChoice(this.imgCord)];
+                const diff = myScore - cpuScore;
+                if(diff === 0){
+                    this.result = '비겼습니다.';
+                } else if([-1,2].includes(diff)){
+                    this.result = '이겼습니다.';
+                    this.score += 1;
+                } else{
+                    this.result = '졌습니다.';
+                    this.score -= 1;
+                }
+                setTimeout(() => {
+                    this.changeHand();
+                }, 1000);
             }
         },
 
         mounted() {
             console.log('mounted');
-            interval = setInterval(() => {
-                if(this.imgCord === rspCords.바위){
-                    this.imgCord = rspCords.가위;
-                } else if(this.imgCord === rspCords.가위){
-                    this.imgCord = rspCords.보;
-                } else if(this.imgCord === rspCords.보){
-                    this.imgCord = rspCords.바위;
-                }
-
-            }, 100);
+            this.changeHand();
         },
 
         beforeDestroy() {
             console.log('beforeDestroy');
-          clearInterval(interval);
-        },
-        destroyed() {
+            clearInterval(interval);
         }
     };
 </script>
